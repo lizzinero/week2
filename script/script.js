9db118560b3e1d24014f65e5e8ecf4143c8d710e
lizzinero <lizzinero@gmail.com>
2024-11-07 19:12:51 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function showMessage() {
+    alert('Я учу JavaScript');
+}
+
+const image = document.getElementById('img');
+
+function prev() {
+    image.src = '../images/meme6.jpg';
+}
+
+function next() {
+    image.src = '../images/meme7.jpg';
+}
